faa5206a997315c64e2be40d6b0beff977105f5a
borbier <yuttakhanbaingen@gmail.com>
2020-10-04 13:07:54 +0700

    Add contracts, migration and truffle

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,6 @@
+const TodoList = artifacts.require('TodoList.sol')
+module.exports = function (deployer, network, accounts) {
+  return deployer.then(() => {
+    return deployer.deploy(TodoList)
+  })
+}
